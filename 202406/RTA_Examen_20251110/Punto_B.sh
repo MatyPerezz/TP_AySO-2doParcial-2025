6361c1975e4afdaeeb792df655d1064c961421c8
MatyPerezz <perezabrahammatias@gmail.com>
2025-11-11 00:04:58 -0300

    Punto_B.sh

--- 202406/RTA_Examen_20251110/Punto_B.sh
+++ 202406/RTA_Examen_20251110/Punto_B.sh
@@ -6,3 +6,24 @@ cd ~/RTA_Examen_$(date +%Y%m%d)
 nano Punto_B.sh
 chmod +x Punto_B.sh
 ./Punto_B.sh
+
+sudo groupadd grupo_dev
+sudo groupadd grupo_test
+sudo groupadd grupo_ops
+sudo useradd -m -G grupo_dev usuario1
+sudo useradd -m -G grupo_test usuario2
+sudo useradd -m -G grupo_ops usuario3
+sudo mkdir -p /work/devteam /work/testteam /work/opsteam
+sudo chown usuario1:grupo_dev /work/devteam
+sudo chown usuario2:grupo_test /work/testteam
+sudo chown usuario3:grupo_ops /work/opsteam
+sudo chmod 770 /work/devteam
+sudo chmod 750 /work/testteam
+sudo chmod 700 /work/opsteam
+sudo -u usuario1 touch /work/devteam/dev1.txt
+sudo -u usuario2 touch /work/testteam/test1.txt
+sudo -u usuario3 touch /work/opsteam/ops1.txt
+ls -l /work
+ls -l /work/devteam
+ls -l /work/testteam
+ls -l /work/opsteam
